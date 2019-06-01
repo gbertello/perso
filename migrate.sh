9f93d80126631a629dde4eabf7f970a2b4b0f7f6
Guillaume BERTELLO <guillaume.bertello@amadeus.com>
2019-06-02 00:58:07 +0200

    Remove migration script

--- migrate.sh
+++
@@ -1,5 +0,0 @@
-ssh $OVH "mkdir -p ~/perso/mongo/data/prod"
-ssh $OVH "mkdir -p ~/perso/node/public/img/prod"
-
-scp -r mongo/data/prod $OVH:~/perso/mongo/data/prod
-scp -r node/public/img/prod $OVH:~/perso/node/public/img/prod
