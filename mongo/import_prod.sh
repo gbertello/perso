e134a8b116c67fc67e463297734e95f9b88fb872
Guillaume BERTELLO <guillaume.bertello@amadeus.com>
2019-06-06 20:52:37 +0200

    Add personal site

--- mongo/import_prod.sh
+++ mongo/import_prod.sh
@@ -1,5 +1,5 @@
 BASEDIR=$(dirname "$0")
 
 docker cp "$BASEDIR/data/prod/cv.json" perso_mongo_prod:/tmp/cv.json 
-docker exec -it perso_mongo_prod mongoimport --db cv --collection cv --file /tmp/cv.json
+docker exec -it perso_mongo_prod mongoimport --db cv --collection cv --drop --file /tmp/cv.json
 docker exec -it perso_mongo_prod rm /tmp/cv.json
