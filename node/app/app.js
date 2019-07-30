f660df197ea34ec1c8a9ab5bafdfe2f6a85f9fde
Guillaume BERTELLO <guillaume.bertello@amadeus.com>
2019-07-30 22:56:55 +0200

    Refactor for reverse proxy

--- node/app/app.js
+++ node/app/app.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var path = require('path');
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
 var MongoClient = require('mongodb').MongoClient;
 var mongoUrl = 'mongodb://mongo_' + process.env.ENV + ':27017/';
 
@@ -11,19 +8,19 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/storage', express.static(path.join(__dirname, 'storage')));
+app.use('/perso/public', express.static(path.join(__dirname, 'public')));
+app.use('/perso/storage', express.static(path.join(__dirname, 'storage')));
 
 var basicAuth = require('express-basic-auth');
 var users = [];
 users[process.env.USERNAME] = process.env.PASSWORD;
 var myAuth = basicAuth({challenge: true, users: users});
 
-app.get('/', function(req, res){
-  res.redirect('/CV');
+app.get('/perso', function(req, res){
+  res.redirect('/perso/CV');
 });
 
-app.get('/CV', function(req, res){
+app.get('/perso/CV', function(req, res){
   MongoClient.connect(mongoUrl, function(err, db) {
     if (err) throw err;
     db.db("perso").collection("cv").find().toArray(function(err, result) {
@@ -33,7 +30,7 @@ app.get('/CV', function(req, res){
   });
 });
 
-app.get('/CoverLetter', myAuth, function(req, res){
+app.get('/perso/CoverLetter', myAuth, function(req, res){
   MongoClient.connect(mongoUrl, function(err, db) {
     if (err) throw err;
     db.db("perso").collection("cv").find().toArray(function(err, result) {
@@ -46,7 +43,7 @@ app.get('/CoverLetter', myAuth, function(req, res){
   });
 });
 
-app.get('/Loisirs', function(req, res){
+app.get('/perso/Loisirs', function(req, res){
   MongoClient.connect(mongoUrl, function(err, db) {
     if (err) throw err;
     db.db("perso").collection("loisirs").find().toArray(function(err, result) {
@@ -56,28 +53,4 @@ app.get('/Loisirs', function(req, res){
   });
 });
 
-if (process.env.ENV == 'prod') {
-  var privateKey = fs.readFileSync('certs/privkey.pem', 'utf8');
-  var certificate = fs.readFileSync('certs/cert.pem', 'utf8');
-  var ca = fs.readFileSync('certs/chain.pem', 'utf8');
-
-  var credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca
-  };
-
-  var httpsServer = https.createServer(credentials, app);
-  httpsServer.listen(443);
-
-  var appHttp = express();
-  appHttp.get('*', function(req, res) {  
-      res.redirect('https://' + req.headers.host + req.url);
-  })
-  
-  var httpServer = http.createServer(appHttp);
-  httpServer.listen(80);
-} else {
-  var httpServer = http.createServer(app);
-  httpServer.listen(80);
-}
+app.listen(80);
